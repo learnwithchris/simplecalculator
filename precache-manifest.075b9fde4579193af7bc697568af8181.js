fb5a3751a4eb4442b88e8c0bd739b925c67b62a7
learnwithchris <56309725+learnwithchris@users.noreply.github.com>
2019-11-15 08:38:28 -0500

    Updates

---
+++ precache-manifest.075b9fde4579193af7bc697568af8181.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ebf0e377e900a85005931b4f85161b",
+    "url": "/simplecalculator/index.html"
+  },
+  {
+    "revision": "d4a05bad08ddea30eef2",
+    "url": "/simplecalculator/static/css/main.2a7c4b77.chunk.css"
+  },
+  {
+    "revision": "9a798451ccbb4d86f48c",
+    "url": "/simplecalculator/static/js/2.0edbd314.chunk.js"
+  },
+  {
+    "revision": "d4a05bad08ddea30eef2",
+    "url": "/simplecalculator/static/js/main.85a0c9ec.chunk.js"
+  },
+  {
+    "revision": "a6b5a6e2ece7109105c4",
+    "url": "/simplecalculator/static/js/runtime-main.a5c8806d.js"
+  }
+]);
